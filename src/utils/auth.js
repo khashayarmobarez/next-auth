ee78394ff820f531bc8304353dc4170c6fcbb4be
khashayar <khashayarmobarezbusiness@gmail.com>
2025-06-25 14:05:56 +0330

    added authentication

---
+++ src/utils/auth.js
@@ -0,0 +1,17 @@
+
+const { hash, compare } = require("bcryptjs")
+
+const hashPasswords = async (password) => {
+    const hashedPassword = await hash(password, 10)
+    return hashedPassword;
+}
+
+const verifyPassword = async (password, hashedPassword) => {
+    const isMatch = await compare(password, hashedPassword)
+    return isMatch;
+}
+
+exports = {
+    hashPasswords,
+    verifyPassword
+}
