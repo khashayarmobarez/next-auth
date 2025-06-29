1f351fe6577b06647ff0d13defbe3cf979207444
khashayar <khashayarmobarezbusiness@gmail.com>
2025-06-29 18:23:53 +0330

    fixing the login request from next auth

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
+'use client'
 import Image from "next/image";
+import { signIn } from 'next-auth/react'
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div >
+    <div className="flex flex-col items-start p-10 min-h-screen gap-y-2">
       <h1>next-auth credentials</h1>
-      <button>
+      <button className="bg-blue-500 text-white p-2 rounded-md">
         <Link href={'/signUp'}>Register</Link>
+      </button>
+      <button
+       className="bg-blue-500 text-white p-2 rounded-md"
+       onClick={() => signIn()}
+      >
+        Login
       </button>
     </div>
   );
